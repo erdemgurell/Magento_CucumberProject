2d972ac31aef12bcd92fd4f9d53af4e69dab0446
Artam <erdemmmgurel@gmail.com>
2024-04-15 18:03:14 +0300

    US701_Registration.feature > Completed

---
+++ src/test/java/pages/US701_Content.java
@@ -0,0 +1,30 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.GWD;
+
+import java.util.List;
+
+public class US701_Content {
+
+    public US701_Content() {
+        PageFactory.initElements(GWD.getDriver(), this);
+    }
+
+
+    @FindBy(linkText = "Create an Account")
+    public WebElement createAccountBtn;
+
+    @FindBy(css = "button[title=\"Create an Account\"]")
+    public WebElement submitRegistrationBtn;
+
+    @FindBy(css = "form[id=\"form-validate\"] div input")
+    public List<WebElement> registrationForm;
+
+    @FindBy(xpath = "//div[contains(text(),\"Thank you\")]")
+    public WebElement accCreatedSuccessMessage;
+
+
+}
--- src/test/java/stepDefinitions/RegistrationSteps.java
+++ src/test/java/stepDefinitions/RegistrationSteps.java
@@ -1,13 +1,43 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import pages.Parent;
+import pages.US701_Content;
 import utilities.ConfigReader;
 import utilities.GWD;
 
 public class RegistrationSteps extends Parent {
+    US701_Content us1 = new US701_Content();
+
     @Given("Navigate to Magento website")
     public void navigateToMagentoWebsite() {
         GWD.getDriver().get(ConfigReader.getProperty("URL"));
+    }
+
+    @When("Click on the Create an Account button")
+    public void clickOnTheCreateAnAccountButton() {
+        myClick(us1.createAccountBtn);
+    }
+
+    @Then("Fill the registration information")
+    public void fillTheRegistrationInformation() {
+        mySendKeys(us1.registrationForm.get(0), "Artam");
+        mySendKeys(us1.registrationForm.get(1), "Soei");
+        mySendKeys(us1.registrationForm.get(2), "lauro_oswaldzeye@delaware.ll");
+        mySendKeys(us1.registrationForm.get(3), "UdBMm88gjg0");
+        mySendKeys(us1.registrationForm.get(4), "UdBMm88gjg0");
+    }
+
+    @And("Click on the submit registration button")
+    public void clickOnTheSubmitRegistrationButton() {
+        myClick(us1.submitRegistrationBtn);
+    }
+
+    @And("User should see the success message")
+    public void userShouldSeeTheSuccessMessage() {
+        verifyContainsText(us1.accCreatedSuccessMessage, "Thank you");
     }
 }
