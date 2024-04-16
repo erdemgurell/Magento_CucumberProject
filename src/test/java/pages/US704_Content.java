1a181417711574ac25ca695e92a624b99f5d5b22
Nigar Ayla Ozcanan <158833043+Cognoscentes@users.noreply.github.com>
2024-04-16 14:53:29 +0300

    Added new elements and reorganized code layout

--- src/test/java/pages/US704_Content.java
+++ src/test/java/pages/US704_Content.java
@@ -10,26 +10,80 @@ import java.util.List;
 public class US704_Content extends Parent {
     public US704_Content() {PageFactory.initElements(GWD.getDriver(), this);}
 
-    @FindBy (xpath = "(//div[@id='store.menu']//ul)[1]/li") public WebElement tabMenu;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[1]") public WebElement whatsNew;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[2]") public WebElement women;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[3]") public WebElement men;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[4]") public WebElement gear;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[5]") public WebElement training;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[6]") public WebElement sale;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li") public List<WebElement> womenTopsAndBottoms;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li") public List<WebElement> menTopsAndBottoms;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[4]/ul/li") public List<WebElement> gearSubCategory;
-    @FindBy (xpath = "((//div[@id='store.menu']//ul)[1]/li)[5]/ul/li") public WebElement trainingSubCat;
-    @FindBy (xpath = "(((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[1]/li") public List<WebElement> womenTopsSubCat;
-    @FindBy (xpath = "(((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[2]/li") public List<WebElement> womenBottomsSubCat;
-    @FindBy (xpath = "(((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[1]/li") public List<WebElement> menTopsSubCat;
-    @FindBy (xpath = "(((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[2]/li") public List<WebElement> menBottomsSubCat;
+    @FindBy(xpath = "(//div[@id='store.menu']//ul)[1]/li")
+    public List<WebElement> tabMenuVerification;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[1]")
+    public WebElement whatsNew;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[2]")
+    public WebElement women;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[3]")
+    public WebElement men;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[4]")
+    public WebElement gear;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[5]")
+    public WebElement training;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[6]")
+    public WebElement sale;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li)[1]")
+    public WebElement womenTops;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li)[2]")
+    public WebElement womenBottoms;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li)[1]")
+    public WebElement menTops;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li)[2]")
+    public WebElement menBottoms;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[5]/ul/li")
+    public WebElement trainingSubCat;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li")
+    public List<WebElement> womenTopsAndBottoms;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li")
+    public List<WebElement> menTopsAndBottoms;
+    @FindBy(xpath = "((//div[@id='store.menu']//ul)[1]/li)[4]/ul/li")
+    public List<WebElement> gearSubCategory;
+    @FindBy(css = "[class='level-top ui-corner-all']")
+    public List<WebElement> tabMenu;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[1]/li")
+    public List<WebElement> womenTopsSubCat;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[2]/li")
+    public List<WebElement> womenBottomsSubCat;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[1]/li")
+    public List<WebElement> menTopsSubCat;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[2]/li")
+    public List<WebElement> menBottomsSubCat;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[1]/li)[1]")
+    public WebElement jacketsWm;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[1]/li)[2]")
+    public WebElement hoodiesWm;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[1]/li)[3]")
+    public WebElement teesWm;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[1]/li)[4]")
+    public WebElement brasAndTanksWm;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[2]/li)[1]")
+    public WebElement pantsWm;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[2]/ul/li/ul)[2]/li)[2]")
+    public WebElement shortsWm;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[1]/li)[1]")
+    public WebElement jacketsMn;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[1]/li)[2]")
+    public WebElement hoodiesMn;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[1]/li)[3]")
+    public WebElement teesMn;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[1]/li)[4]")
+    public WebElement tanksMn;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[2]/li)[1]")
+    public WebElement pantsMn;
+    @FindBy(xpath = "((((//div[@id='store.menu']//ul)[1]/li)[3]/ul/li/ul)[2]/li)[2]")
+    public WebElement shortsMn;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[4]/ul/li)[1]")
+    public WebElement bagsGr;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[4]/ul/li)[2]")
+    public WebElement fitnessGr;
+    @FindBy(xpath = "(((//div[@id='store.menu']//ul)[1]/li)[4]/ul/li)[3]")
+    public WebElement watchesGr;
 
     public WebElement getWebElement(String strWebElement){
 
         switch (strWebElement){
-            case "tabMenu"       : return this.tabMenu;
             case "whatsNew"      : return this.whatsNew;
             case "women"         : return this.women;
             case "men"           : return this.men;
@@ -37,11 +91,33 @@ public class US704_Content extends Parent {
             case "training"      : return this.training;
             case "sale"          : return this.sale;
             case "trainingSubCat": return this.trainingSubCat;
+            case "womenTops"     : return this.womenTops;
+            case "womenBottoms"  : return this.womenBottoms;
+            case "menTops"       : return this.menTops;
+            case "menBottoms"    : return this.menBottoms;
+            case "jacketsWm "    : return this.jacketsWm ;
+            case "hoodiesWm "    : return this.hoodiesWm;
+            case "teesWm    "    : return this.teesWm;
+            case "brasAndTanksWm": return this.brasAndTanksWm;
+            case "pantsWm  "     : return this.pantsWm;
+            case "shortsWm "     : return this.shortsWm;
+            case "jacketsMn"     : return this.jacketsMn;
+            case "hoodiesMn"     : return this.hoodiesMn;
+            case "teesMn   "     : return this.teesMn;
+            case "tanksMn  "     : return this.tanksMn;
+            case "pantsMn  "     : return this.pantsMn;
+            case "shortsMn "     : return this.shortsMn;
+            case "bagsGr   "     : return this.bagsGr;
+            case "fitnessGr"     : return this.fitnessGr;
+            case "watchesGr"     : return this.watchesGr;
+
         }
         return null;
-    } public List<WebElement> getListElement(String strListElement){
+    }
+    public List<WebElement> getListElement(String strListElement){
 
         switch (strListElement){
+            case "tabMenuVerification" : return this.tabMenuVerification;
             case "womenTopsAndBottoms" : return this.womenTopsAndBottoms;
             case "menTopsAndBottoms"   : return this.menTopsAndBottoms;
             case "gearSubCategory"     : return this.gearSubCategory;
@@ -49,6 +125,7 @@ public class US704_Content extends Parent {
             case "womenBottomsSubCat"  : return this.womenBottomsSubCat;
             case "menTopsSubCat"       : return this.menTopsSubCat;
             case "menBottomsSubCat"    : return this.menBottomsSubCat;
+            case "tabMenu"             : return this.tabMenu;
         }
         return null;
     }
