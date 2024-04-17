e0ea94522f512d4ddd4379174b5165c8bd35ee84
depom1 <ahmet.36.2000@hotmail.com>
2024-04-17 14:14:19 +0300

    US703_AddAddress.feature initial commit

---
+++ src/test/java/featureFiles/US703_AddAddress.feature
@@ -0,0 +1,7 @@
+Feature: Add Address Functionality
+
+  Background:
+    Given Navigate to Magento website
+    Then Click on the Sign In button
+    And Enter the email and password then click the login button
+    Scenario:
---
+++ src/test/java/pages/US703_Content.java
@@ -0,0 +1,75 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.GWD;
+
+public class US703_Content {
+    public US703_Content(){
+        PageFactory.initElements(GWD.getDriver(),this);
+    }
+
+    @FindBy(linkText = "Sign In")
+    public WebElement signInBtn;
+
+    @FindBy(xpath = "//input[@id='email']")
+    public WebElement email;
+
+    @FindBy(xpath = "//input[@title='Password']")
+    public WebElement password;
+
+    @FindBy(xpath = "//div[@class='primary']/button/span")
+    public WebElement loginBtn;
+
+    @FindBy(xpath = "//span[@class='customer-name']")
+    public WebElement actionsMenu;
+
+    @FindBy(linkText = "My Account")
+    public WebElement myAccountBt;
+
+    @FindBy(linkText = "Address Book")
+    public WebElement adressBook;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='firstname']")
+    public WebElement firstName;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='lastname']")
+    public WebElement lastName;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='company']")
+    public WebElement company;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='telephone']")
+    public WebElement telephone;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='street_1']")
+    public WebElement street_1;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='street_2']")
+    public WebElement street_2;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='street_3']")
+    public WebElement street_3;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='city']")
+    public WebElement city;
+
+    @FindBy(xpath = "//div[@class='control']/input[@id='zip']")
+    public WebElement zip;
+
+    @FindBy(xpath = "//div[@class='control']/select[@id='region_id']")
+    public WebElement state;
+
+    @FindBy(xpath = "//div[@class='control']/select[@id='country']")
+    public WebElement country;
+
+    @FindBy(xpath = "//div[@class='primary']/button[@class='action save primary']")
+    public WebElement saveAdressBtn;
+
+
+
+
+
+
+}
