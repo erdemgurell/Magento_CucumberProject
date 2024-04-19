6676557004ee2707e29e2ef842dece1db90a4f04
beyzanurer <beyzanur.er@outlook.com>
2024-04-19 09:58:11 +0300

    Login Functionality

--- src/test/java/featureFiles/LoginFunctionality.feature
+++ src/test/java/featureFiles/LoginFunctionality.feature
@@ -6,17 +6,16 @@ Feature: Login Functionality
     Then Click on the Sign In button
 
   Scenario: Login Functionality with valid information
+    Given Enter the valid email and password then click the login button
 
-    Given Enter the email and password then click the login button
+  Scenario: Login Functionality with invalid/null information
+    When Enter the null email and password then click the login button
+    And Enter the invalid email and password then click the login button
+    And Enter the email and null password then click the login button
+    Then Enter the email and invalid password then click the login button
 
-  Scenario Outline: Login Functionality with invalid information
-    Given Enter the invalid email and invalid password then click the login button
-    Examples:
-      |<email>  |<password>|
-      |null     |    valid |
-      |invalid  |valid|
-      |valid    |null|
-      |valid    |invalid|
+  Scenario: Forgot your password
+    Given Click on the Forgot your password
 
 
 
--- src/test/java/stepDefinitions/LoginFunctionalitySteps.java
+++ src/test/java/stepDefinitions/LoginFunctionalitySteps.java
@@ -1,10 +1,56 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
 import pages.Parent;
+import pages.LoginPOM;
 
 public class LoginFunctionalitySteps extends Parent {
-    @Given("Enter the invalid email and invalid password then click the login button")
-    public void enterTheInvalidEmailAndInvalidPasswordThenClickTheLoginButton() {
+    LoginPOM us2=new LoginPOM();
+
+    @Given("Enter the valid email and password then click the login button")
+    public void enterTheValidEmailAndPasswordThenClickTheLoginButton() {
+        us2.mySendKeys(us2.email, "gafol82072@agaseo.com");
+        us2.mySendKeys(us2.password, "test123.");
+        us2.myClick(us2.signInBtn);
+        verifyContainsText(us2.succesLogin,"Welcome" );    }
+
+    @Given("Enter the null email and password then click the login button")
+    public void enterTheNullEmailAndPasswordThenClickTheLoginButton() {
+        us2.mySendKeys(us2.email, "");
+        us2.mySendKeys(us2.password, "test123.");
+        us2.myClick(us2.signInBtn);
+        verifyContainsText(us2.nullEmailErrorMessage,"field");
+    }
+
+    @And("Enter the invalid email and password then click the login button")
+    public void enterTheInvalidEmailAndPasswordThenClickTheLoginButton() {
+        us2.mySendKeys(us2.email, "tete@uy.com");
+        us2.mySendKeys(us2.password, "test123.");
+        us2.myClick(us2.signInBtn);
+        verifyContainsText(us2.invalidInformation,"disabled");
+    }
+
+    @And("Enter the email and null password then click the login button")
+    public void enterTheEmailAndNullPasswordThenClickTheLoginButton() {
+        us2.mySendKeys(us2.email, "gafol82072@agaseo.com");
+        us2.mySendKeys(us2.password, "");
+        us2.myClick(us2.signInBtn);
+        verifyContainsText(us2.nullPassword,"field" );
+    }
+
+    @Then("Enter the email and invalid password then click the login button")
+    public void enterTheEmailAndInvalidPasswordThenClickTheLoginButton() {
+        us2.mySendKeys(us2.email, "gafol82072@agaseo.com");
+        us2.mySendKeys(us2.password, "test1");
+        us2.myClick(us2.signInBtn);
+        verifyContainsText(us2.invalidInformation,"disable" );
+    }
+
+    @Given("Click on the Forgot your password")
+    public void clickOnTheForgotYourPassword() {
+        us2.myClick(us2.forgotPasswordBtn);
+        verifyContainsText(us2.passwordPage,"receive");
     }
 }
